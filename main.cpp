ee8af54f207ece96c317ec8af3fe65d97bee7567
Anton Surkis <antonsurkis@gmail.com>
2023-12-07 17:06:49 +0300

    Remove std::optional

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <cstddef>
 #include <fstream>
-#include <optional>
 #include <random>
 
 #include <glm/common.hpp>
@@ -21,6 +20,25 @@ constexpr Real HALF = Real(.5);
 
 ////////////////////////////////////////////////////////////////
 
+constexpr size_t PIC_WIDTH = 1280;
+constexpr size_t PIC_HEIGHT = 720;
+constexpr int PIXEL_MAX = 255;
+
+constexpr int MAX_REFLECTIONS = 16;
+constexpr int AA_SAMPLES = 16;
+
+const Vec3 CAM_ORIGIN = {0., 0., 5.};
+const Vec3 CAM_LOOK_AT = {0., 0., 0.};
+const Vec3 WORLD_UP = {0., 1., 0.};
+
+////////////////////////////////////////////////////////////////
+
+const Vec3 CAM_FORWARD = glm::normalize(CAM_LOOK_AT - CAM_ORIGIN);
+const Vec3 CAM_RIGHT = glm::normalize(glm::cross(CAM_FORWARD, WORLD_UP));
+const Vec3 CAM_UP = glm::normalize(glm::cross(CAM_RIGHT, CAM_FORWARD));
+
+////////////////////////////////////////////////////////////////
+
 struct Hit {
     Vec3 point;
     Vec3 normal;
@@ -33,7 +51,7 @@ Vec3 DebugNormal(Vec3 norm) { return norm * HALF + HALF; }
 
 ////////////////////////////////////////////////////////////////
 
-std::optional<Hit> ScanSphereO(Vec3 dir, Vec3 oa, Real radius) {
+bool ScanSphereO(Hit &outHit, Vec3 dir, Vec3 oa, Real radius) {
     // O --- центр координат
     // A --- центр сферы
     // H --- проекция центра сферы на луч
@@ -44,50 +62,47 @@ std::optional<Hit> ScanSphereO(Vec3 dir, Vec3 oa, Real radius) {
     Real ah2 = glm::dot(ah, ah);
     Real r2 = radius * radius;
     Real hx2 = r2 - ah2;
-    if (hx2 < ZERO) return {};
+    if (hx2 < ZERO) return false;
 
     Real oxLen = glm::length(oh) - glm::sqrt(hx2);
     Vec3 ox = dir * oxLen;
     Vec3 ax = ox - oa;
     Vec3 norm = glm::normalize(ax);
 
-    if (oxLen < ZERO) return {};
-    if (glm::dot(norm, dir) > ZERO) return {};
+    if (oxLen < ZERO) return false;
+    if (glm::dot(norm, dir) > ZERO) return false;
 
-    return {{ox, norm, oxLen}};
+    outHit.point = ox;
+    outHit.normal = norm;
+    outHit.distance = oxLen;
+    return true;
 }
 
-std::optional<Hit> ScanSphere(Vec3 origin, Vec3 dir, Vec3 center, Real radius) {
-    std::optional<Hit> hit = ScanSphereO(dir, center - origin, radius);
-    if (hit.has_value()) { hit->point += origin; }
-    return hit;
+bool ScanSphere(Hit &outHit, Vec3 origin, Vec3 dir, Vec3 center, Real radius) {
+    bool hitFound = ScanSphereO(outHit, dir, center - origin, radius);
+    outHit.point += origin;
+    return hitFound;
 }
 
 ////////////////////////////////////////////////////////////////
 
-const Vec3 CAM_ORIGIN = {0., 0., 5.};
-const Vec3 CAM_LOOK_AT = {0., 0., 0.};
-const Vec3 WORLD_UP = {0., 1., 0.};
-
-const Vec3 CAM_FORWARD = glm::normalize(CAM_LOOK_AT - CAM_ORIGIN);
-const Vec3 CAM_RIGHT = glm::normalize(glm::cross(CAM_FORWARD, WORLD_UP));
-const Vec3 CAM_UP = glm::normalize(glm::cross(CAM_RIGHT, CAM_FORWARD));
-
-std::optional<Hit> ScanScene(Vec3 origin, Vec3 dir) {
-    std::optional<Hit> bestHit;
+bool ScanScene(Hit &bestHit, Vec3 origin, Vec3 dir) {
+    bool bestHitFound = false;
     for (int i = -2; i < 2; ++i) {
         for (int j = -2; j < 2; ++j) {
             for (int k = -2; k < 2; ++k) {
                 Vec3 pos = Real(3.) * (Vec3(i, j, k) + HALF);
-                std::optional<Hit> hit = ScanSphere(origin, dir, pos, ONE);
-                if (!hit) continue;
-                if (!bestHit || bestHit->distance > hit->distance) {
+                Hit hit;
+                bool hitFound = ScanSphere(hit, origin, dir, pos, ONE);
+                if (!hitFound) continue;
+                if (!bestHitFound || bestHit.distance > hit.distance) {
                     bestHit = hit;
+                    bestHitFound = true;
                 }
             }
         }
     }
-    return bestHit;
+    return bestHitFound;
 }
 
 ////////////////////////////////////////////////////////////////
@@ -100,22 +115,20 @@ Vec3 Sky(Vec3 dir) {
     return DebugNormal(dir);
 }
 
-constexpr int MAX_REFLECTIONS = 4;
-
 Vec3 CastRay(Vec3 origin, Vec3 dir) {
     Vec3 acc = {};
     Real weight = ONE;
     for (int i = 0; i < MAX_REFLECTIONS; ++i) {
-        std::optional<Hit> hit = ScanScene(origin, dir);
-        if (!hit) break;
+        Hit hit;
+        bool hitFound = ScanScene(hit, origin, dir);
+        if (!hitFound) break;
 
         Real reflectivity = .75;
         acc += weight * (1. - reflectivity) * .25;
         weight *= reflectivity;
 
-        origin = hit->point;
-        dir = glm::reflect(dir, hit->normal);
-        // return DebugNormal(hit->normal);
+        origin = hit.point;
+        dir = glm::reflect(dir, hit.normal);
     }
     acc += weight * Sky(dir);
     return acc;
@@ -123,15 +136,11 @@ Vec3 CastRay(Vec3 origin, Vec3 dir) {
 
 ////////////////////////////////////////////////////////////////
 
-constexpr size_t PIC_WIDTH = 640;
-constexpr size_t PIC_HEIGHT = 480;
-
 Vec3 picture[PIC_HEIGHT][PIC_WIDTH];
 
 void GeneratePicture() {
     constexpr Real ASPECT_RATIO = Real(PIC_WIDTH) / Real(PIC_HEIGHT);
-    constexpr int SAMPLES = 64;
-    constexpr Real SAMPLE_WEIGHT = ONE / SAMPLES;
+    constexpr Real SAMPLE_WEIGHT = ONE / AA_SAMPLES;
 
 #pragma omp parallel for
     for (size_t row = 0; row < PIC_HEIGHT; ++row) {
@@ -140,7 +149,7 @@ void GeneratePicture() {
 
         for (size_t col = 0; col < PIC_WIDTH; ++col) {
             Vec3 result = {};
-            for (int i = 0; i < SAMPLES; ++i) {
+            for (int i = 0; i < AA_SAMPLES; ++i) {
                 Vec2 xy = Vec2(col, row);
                 xy.x += uniform(rng);
                 xy.y += uniform(rng);
@@ -160,12 +169,12 @@ void GeneratePicture() {
 
 void WritePicture() {
     std::ofstream fout("out.ppm");
-    fout << "P3 " << PIC_WIDTH << ' ' << PIC_HEIGHT << " 255\n";
+    fout << "P3 " << PIC_WIDTH << ' ' << PIC_HEIGHT << ' ' << PIXEL_MAX << '\n';
     for (size_t row = 0; row < PIC_HEIGHT; ++row) {
         for (size_t col = 0; col < PIC_WIDTH; ++col) {
             for (size_t chan = 0; chan < 3; ++chan) {
-                int asInt = int(picture[row][col][chan] * 256);
-                asInt = std::min(std::max(asInt, 0), 255);
+                int asInt = int(picture[row][col][chan] * (PIXEL_MAX + 1));
+                asInt = std::min(std::max(asInt, 0), PIXEL_MAX);
                 fout << asInt << ' ';
             }
         }
