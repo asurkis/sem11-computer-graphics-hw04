efd8192f6036f08511a6f338345151d1001485c7
Anton Surkis <antonsurkis@gmail.com>
2023-12-07 19:46:40 +0300

    Add checkerboard pattern on the floor

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ using Real = float;
 using Vec2 = glm::vec<2, Real>;
 using Vec3 = glm::vec<3, Real>;
 using Vec4 = glm::vec<4, Real>;
+using Vec2i = glm::vec<2, int>;
+using Vec3i = glm::vec<3, int>;
+using Vec4i = glm::vec<4, int>;
 
 constexpr Real ZERO = Real(0.);
 constexpr Real ONE = Real(1.);
@@ -24,11 +27,11 @@ constexpr size_t PIC_WIDTH = 1280;
 constexpr size_t PIC_HEIGHT = 720;
 constexpr int PIXEL_MAX = 255;
 
-constexpr int MAX_REFLECTIONS = 16;
-constexpr int AA_SAMPLES = 16;
+constexpr int MAX_REFLECTIONS = 4;
+constexpr int AA_SAMPLES = 4;
 
-const Vec3 CAM_ORIGIN = {0., 0., 5.};
-const Vec3 CAM_LOOK_AT = {0., 0., 0.};
+const Vec3 CAM_ORIGIN = {0., 1., 5.};
+const Vec3 CAM_LOOK_AT = {0., 2., 0.};
 const Vec3 WORLD_UP = {0., 1., 0.};
 
 ////////////////////////////////////////////////////////////////
@@ -39,24 +42,40 @@ const Vec3 CAM_UP = glm::normalize(glm::cross(CAM_RIGHT, CAM_FORWARD));
 
 ////////////////////////////////////////////////////////////////
 
-struct Hit {
-    Vec3 point;
-    Vec3 normal;
-    Real distance;
+struct HitPoint {
+    Vec3 Position;
+    Vec3 Normal;
+    Vec2 TexCoord;
+    Real Distance;
+};
+
+struct HitMaterial {
+    Vec3 BaseColor;
+    Real Reflectivity;
+};
+
+struct HitFull {
+    HitPoint Point;
+    HitMaterial Material;
+    bool Found;
 };
 
 ////////////////////////////////////////////////////////////////
 
-Vec3 DebugNormal(Vec3 norm) { return norm * HALF + HALF; }
+Vec3 DebugNormal(Vec3 norm) noexcept { return norm * HALF + HALF; }
 
 ////////////////////////////////////////////////////////////////
 
-bool ScanSphereO(Hit &outHit, Vec3 dir, Vec3 oa, Real radius) {
+// Всегда отсекаем внутренние поверхности,
+// они мешают, когда отражаем лучи.
+
+bool ScanSphereO(HitPoint &out, Vec3 dir, Vec3 oa, Real radius) noexcept {
     // O --- центр координат
     // A --- центр сферы
     // H --- проекция центра сферы на луч
     // X --- точка пересечения луча со сферой
-    // dot(OH, AH) = 0
+    // X лежит на OH
+    // OHA --- прямой угол
     Vec3 oh = dir * glm::dot(dir, oa);
     Vec3 ah = oh - oa;
     Real ah2 = glm::dot(ah, ah);
@@ -70,44 +89,90 @@ bool ScanSphereO(Hit &outHit, Vec3 dir, Vec3 oa, Real radius) {
     Vec3 norm = glm::normalize(ax);
 
     if (oxLen < ZERO) return false;
-    if (glm::dot(norm, dir) > ZERO) return false;
+    if (glm::dot(norm, dir) >= ZERO) return false;
 
-    outHit.point = ox;
-    outHit.normal = norm;
-    outHit.distance = oxLen;
+    out.Position = ox;
+    out.Normal = norm;
+    out.Distance = oxLen;
+
     return true;
 }
 
-bool ScanSphere(Hit &outHit, Vec3 origin, Vec3 dir, Vec3 center, Real radius) {
-    bool hitFound = ScanSphereO(outHit, dir, center - origin, radius);
-    outHit.point += origin;
+bool ScanSphere(HitPoint &out, Vec3 origin, Vec3 dir, Vec3 center,
+                Real radius) noexcept {
+    bool hitFound = ScanSphereO(out, dir, center - origin, radius);
+    if (hitFound) out.Position += origin;
+    return hitFound;
+}
+
+bool ScanPlaneO(HitPoint &out, Vec3 dir, Vec3 oa, Vec3 planeX, Vec3 planeY,
+                Vec3 planeZ) noexcept {
+    // O --- центр координат
+    // A --- точка на плоскости
+    // X --- точка пересечения луча с плоскостью
+    Real dirDotNorm = glm::dot(dir, planeY);
+    if (dirDotNorm >= ZERO) return false;
+
+    out.Distance = -ONE / dirDotNorm;
+    out.Position = dir * out.Distance;
+    out.Normal = planeY;
+
+    Vec3 ax = out.Position - oa;
+    out.TexCoord.x = glm::dot(ax, planeX);
+    out.TexCoord.y = glm::dot(ax, planeZ);
+
+    return true;
+}
+
+bool ScanPlane(HitPoint &out, Vec3 origin, Vec3 dir, Vec3 planeOrigin,
+               Vec3 planeX, Vec3 planeY, Vec3 planeZ) noexcept {
+    bool hitFound
+        = ScanPlaneO(out, dir, planeOrigin - origin, planeX, planeY, planeZ);
+    if (hitFound) out.Position += origin;
     return hitFound;
 }
 
 ////////////////////////////////////////////////////////////////
 
-bool ScanScene(Hit &bestHit, Vec3 origin, Vec3 dir) {
-    bool bestHitFound = false;
+void HitJoin(HitFull &lhsHit, const HitFull &rhsHit) noexcept {
+    if (!rhsHit.Found) return;
+    if (!lhsHit.Found || rhsHit.Point.Distance < lhsHit.Point.Distance) {
+        lhsHit = rhsHit;
+    }
+}
+
+void ScanScene(HitFull &bestHit, Vec3 origin, Vec3 dir) noexcept {
     for (int i = -2; i < 2; ++i) {
-        for (int j = -2; j < 2; ++j) {
+        for (int j = 0; j < 4; ++j) {
             for (int k = -2; k < 2; ++k) {
                 Vec3 pos = Real(3.) * (Vec3(i, j, k) + HALF);
-                Hit hit;
-                bool hitFound = ScanSphere(hit, origin, dir, pos, ONE);
-                if (!hitFound) continue;
-                if (!bestHitFound || bestHit.distance > hit.distance) {
-                    bestHit = hit;
-                    bestHitFound = true;
-                }
+                HitFull hit;
+                hit.Found = ScanSphere(hit.Point, origin, dir, pos, ONE);
+                hit.Material.BaseColor = Vec3(.25);
+                hit.Material.Reflectivity = .75;
+                HitJoin(bestHit, hit);
             }
         }
     }
-    return bestHitFound;
+
+    HitFull floorHit;
+    floorHit.Found = ScanPlane(floorHit.Point, origin, dir, {}, {1., 0., 0.},
+                               {0., 1., 0.}, {0., 0., 1.});
+    if (floorHit.Found) {
+        Vec2i ij = glm::floor(floorHit.Point.TexCoord);
+        if ((ij.x + ij.y) % 2 == 0) {
+            floorHit.Material.BaseColor = Vec3(.8);
+        } else {
+            floorHit.Material.BaseColor = Vec3(.2);
+        }
+        floorHit.Material.Reflectivity = .25;
+    }
+    HitJoin(bestHit, floorHit);
 }
 
 ////////////////////////////////////////////////////////////////
 
-Vec3 Sky(Vec3 dir) {
+Vec3 Sky(Vec3 dir) noexcept {
     // Для отладки покажем нормаль
     // Real max0 = std::max(ZERO, dir.y);
     // Real gray = max0 * max0;
@@ -115,20 +180,21 @@ Vec3 Sky(Vec3 dir) {
     return DebugNormal(dir);
 }
 
-Vec3 CastRay(Vec3 origin, Vec3 dir) {
+Vec3 CastRay(Vec3 origin, Vec3 dir) noexcept {
     Vec3 acc = {};
     Real weight = ONE;
     for (int i = 0; i < MAX_REFLECTIONS; ++i) {
-        Hit hit;
-        bool hitFound = ScanScene(hit, origin, dir);
-        if (!hitFound) break;
+        HitFull hit = {};
+        ScanScene(hit, origin, dir);
+        if (!hit.Found) break;
 
-        Real reflectivity = .75;
-        acc += weight * (1. - reflectivity) * .25;
+        Vec3 base = hit.Material.BaseColor;
+        Real reflectivity = hit.Material.Reflectivity;
+        acc += weight * (ONE - reflectivity) * base;
         weight *= reflectivity;
 
-        origin = hit.point;
-        dir = glm::reflect(dir, hit.normal);
+        origin = hit.Point.Position;
+        dir = glm::reflect(dir, hit.Point.Normal);
     }
     acc += weight * Sky(dir);
     return acc;
@@ -138,7 +204,7 @@ Vec3 CastRay(Vec3 origin, Vec3 dir) {
 
 Vec3 picture[PIC_HEIGHT][PIC_WIDTH];
 
-void GeneratePicture() {
+void GeneratePicture() noexcept {
     constexpr Real ASPECT_RATIO = Real(PIC_WIDTH) / Real(PIC_HEIGHT);
     constexpr Real SAMPLE_WEIGHT = ONE / AA_SAMPLES;
 
